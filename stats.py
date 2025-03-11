afe78bf81b818f3d9912f8a0177f52a50797005a
paako <bootdev@dsts.info>
2025-03-11 12:46:20 +0000

    Change report format

--- stats.py
+++ stats.py
@@ -8,11 +8,13 @@ def bookreport(path_to_file, words, chars):
             alphabet_count.append({"character": k, "count": chars[k]})
     alphabet_count.sort(reverse=True, key=sort_on)
     
-    print(f"--- Begin report of {path_to_file} ---")
-    print(f"{len(words)} words was found in the document")
-    print()
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {path_to_file}")
+    print("----------- Word Count ----------")
+    print(f"Found {len(words)} total words")
+    print("--------- Character Count -------")
     for k in alphabet_count:
         char = k['character']
         count = k['count']
-        print(f"The '{char}' character was found {count} times.")
-    print("--- End report ---")
+        print(f"{char}: {count}")
+    print("============= END ===============")
